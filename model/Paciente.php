4256a9eba59969b7b8de32b815ea535dd6e29d30
leonardo-barros1110 <leonardobarros@aluno.senai.br>
2024-10-30 10:36:44 -0300

    atualização
    atualiza

--- model/Paciente.php
+++ model/Paciente.php
@@ -19,12 +19,6 @@ class Paciente {
         $query = "INSERT INTO " . $this->table . " (nome, data_nascimento, cpf, convenio, telefone) VALUES (:nome, :data_nascimento, :cpf, :convenio, :telefone)";
         $stmt = $this->conn->prepare($query);
 
-        $this->nome = htmlspecialchars(strip_tags($this->nome));
-        $this->data_nascimento = htmlspecialchars(strip_tags($this->data_nascimento));
-        $this->cpf = htmlspecialchars(strip_tags($this->cpf));
-        $this->convenio = htmlspecialchars(strip_tags($this->convenio));
-        $this->telefone = htmlspecialchars(strip_tags($this->telefone));
-
         $stmt->bindParam(":nome", $this->nome);
         $stmt->bindParam(":data_nascimento", $this->data_nascimento);
         $stmt->bindParam(":cpf", $this->cpf);
@@ -35,7 +29,14 @@ class Paciente {
             return true;
         }
 
+
         return false;
+    }
+    public function getall(){
+        $query = "SELECT * FROM" . $this->table;
+        $stmt = $this->conn->prepare(($query));
+        $stmt ->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
 ?>
